bae51d43071e533384d341b4bf7c25831c9b9357
youngeuni <young53967@gmail.com>
2017-08-20 12:35:43 +0900

    Add SettingActivity
    1) add SettingActivity

--- app/src/main/java/yapp11th/devcamp/co/kr/rebuilding01/SettingActivity.java
+++ app/src/main/java/yapp11th/devcamp/co/kr/rebuilding01/SettingActivity.java
@@ -2,6 +2,7 @@ package yapp11th.devcamp.co.kr.rebuilding01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Button b;
     }
 }
