b13ae4d4514de383b55f0d283e060b178d8b419c
GRAMGRAM\hayoung <hy21116@naver.com>
2017-09-12 01:22:00 +0900

    Update Project
    1) hayoung github checking

--- app/src/main/java/yapp11th/devcamp/co/kr/rebuilding01/Statistics/ChartActivity.java
+++ app/src/main/java/yapp11th/devcamp/co/kr/rebuilding01/Statistics/ChartActivity.java
@@ -7,6 +7,7 @@ import yapp11th.devcamp.co.kr.rebuilding01.R;
 
 public class ChartActivity extends AppCompatActivity {
 
+    // 하영쓰는 대단한 사람이다.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
